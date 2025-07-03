458cd1c0c0d59af407ba2d2c8b16d9be88475e5b
Rohit88435 <kuprajapatir@gmail.com>
2025-07-03 12:15:43 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -33,7 +33,7 @@ export const signUp = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: "true",
       secure: (process.env.NODE_ENVIRONMENT = "production"),
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -65,7 +65,7 @@ export const login = async (req, res) => {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
       secure: (process.env.NODE_ENVIRONMENT = "production"),
-      sameSite: "strict",
+      sameSite: "none",
     });
 
     return res.status(200).json(user);
